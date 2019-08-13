98567dd29bdd0871eeedea7e62dab206ce1b93c3
Abby Chao <abigail.chao@gmail.com>
2019-08-13 16:57:39 -0400

    Simplified file structure to separate HTTP SSE vs others

---
+++ test-server/wss.js
@@ -0,0 +1,45 @@
+
+/*
+WEB SOCKET
+*/
+
+const wss = new WebSocket.Server({
+  port: 5000,
+});
+
+function heartbeat() {
+  this.isAlive = true;
+}
+
+wss.on('connection', (wsClient) => {
+  wsClient.send('You are connected to WS.');
+  wsClient.isAlive = true;
+  wsClient.on('pong', heartbeat);
+
+  wsClient.on('message', (message) => {
+    console.log('received message');
+
+    wss.clients.forEach(client => {
+      client.send('Echo: ' + message);
+    })
+  })
+});
+
+// broadcast constantly
+// setInterval(() => {
+//   wss.clients.forEach(client => {
+//     client.send("Hi from server.");
+//   })
+// }, 2000)
+
+//ping
+setInterval(() => {
+  wss.clients.forEach(client => {
+    if (client.isAlive === false) {
+      return client.terminate();
+    }
+
+    client.isAlive = false;
+    client.ping(() => { });
+  })
+}, 10000)
