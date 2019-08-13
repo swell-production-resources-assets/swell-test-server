62aee33c82fece93c107a20f6f0c76f4ef1154c1
Abby Chao <abigail.chao@gmail.com>
2019-08-13 16:38:24 -0400

    Updated for Heroku

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Swell-test-server
+
+{change package.json script to "server": "nodemon test-server/server.js"}
+To test WS -> WS localhost:5000
+To test SSE1 -> HTTP http://localhost:80/events
+To test SSE2 -> HTTPS https://localhost:8443
+
+------------
+
+{change package.json script to "server": "nodemon test-server/index.js"}
+To test index.js SSE3 -> HTTP http://localhost:8888/events
--- test-server/server.js
+++ test-server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const WebSocket = require ('ws');
+const WebSocket = require('ws');
 
 
 const app = express();
@@ -10,60 +10,61 @@ const path = require("path");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
-app.listen(80, "0.0.0.0", () => {
-  console.log("Express server on 80, 0.0.0.0");
+// If deployed to Heroku, choose port for listening dynamically
+app.listen(process.env.PORT || 80, "0.0.0.0", () => {
+  console.log(`Express server on ${process.env.PORT || 80}, 0.0.0.0`);
 });
 
-let eventsArr = [{body:'test'},{body:'test'},{body:'test'},{body:'test'},{body:'test'},{body:'test'},{body:'test'},{body:'test'},{body:'test'},{body:'test'}];
+let eventsArr = [{ body: 'test' }, { body: 'test' }, { body: 'test' }, { body: 'test' }, { body: 'test' }, { body: 'test' }, { body: 'test' }, { body: 'test' }, { body: 'test' }, { body: 'test' }];
 
 app.get('/events', (req, res) => {
-    res.header('Content-Type', 'text/event-stream');
-    res.header('Cache-Control', 'no-cache');
-    res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true });
-    res.cookie('cookie2', 'i\'m-a-good-little-cookie')
+  res.header('Content-Type', 'text/event-stream');
+  res.header('Cache-Control', 'no-cache');
+  res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true });
+  res.cookie('cookie2', 'i\'m-a-good-little-cookie')
 
   let id = 0;
 
-    setInterval(() => {
-        if(eventsArr.length > id) {
-            res.write('event: my-custom-event\n');
-            res.write('id: ' + id + '\n');
-            res.write('data :' + JSON.stringify(eventsArr[id])+'\n\n');
-            id++;
-        }
-    }, 25)
+  setInterval(() => {
+    if (eventsArr.length > id) {
+      res.write('event: my-custom-event\n');
+      res.write('id: ' + id + '\n');
+      res.write('data :' + JSON.stringify(eventsArr[id]) + '\n\n');
+      id++;
+    }
+  }, 25)
 
-    // setInterval(function() {
-    //     res.write('event: my-custom-event\n');
-    //     res.write('id: ' + id++ + '\n');
-    //     res.write(`data: Your headers: ${JSON.stringify(req.headers)}\n`);
-    //     res.write('\n\n')
-    // }, 2000);
+  // setInterval(function() {
+  //     res.write('event: my-custom-event\n');
+  //     res.write('id: ' + id++ + '\n');
+  //     res.write(`data: Your headers: ${JSON.stringify(req.headers)}\n`);
+  //     res.write('\n\n')
+  // }, 2000);
 });
 
 app.post('/events', (req, res) => {
-    console.log('headers', req.headers);
-    console.log("body", req.body);
-    res.header('Cache-Control', 'no-cache');
-    res.cookie('posted', 'you-sent-a-cookie-after-posting!')
+  console.log('headers', req.headers);
+  console.log("body", req.body);
+  res.header('Cache-Control', 'no-cache');
+  res.cookie('posted', 'you-sent-a-cookie-after-posting!')
 
-    eventsArr.push(req.body);
-    res.end('Body Received: ' + JSON.stringify(req.body));
+  eventsArr.push(req.body);
+  res.end('Body Received: ' + JSON.stringify(req.body));
 
-    // let id=0;
-    // setInterval(function() {
-    //     res.write('event: my-custom-event\n');
-    //     res.write('id: ' + id++ + '\n');
-    //     res.write(`data: Your headers: ${JSON.stringify(req.headers)}\n`);
-    //     res.write(`data: Your body: ${JSON.stringify(req.body)}\n`);
-    //     res.write('\n\n')
-    // }, 2500);
+  // let id=0;
+  // setInterval(function() {
+  //     res.write('event: my-custom-event\n');
+  //     res.write('id: ' + id++ + '\n');
+  //     res.write(`data: Your headers: ${JSON.stringify(req.headers)}\n`);
+  //     res.write(`data: Your body: ${JSON.stringify(req.body)}\n`);
+  //     res.write('\n\n')
+  // }, 2500);
 });
 
 
@@ -71,45 +72,45 @@ app.post('/events', (req, res) => {
 WEB SOCKET
 */
 
-const wss = new WebSocket.Server({ 
-    port: 5000,
+const wss = new WebSocket.Server({
+  port: 5000,
 });
 
-function heartbeat () {
-    this.isAlive = true;
+function heartbeat() {
+  this.isAlive = true;
 }
 
-wss.on('connection',  (wsClient) => {
-    wsClient.send('You are connected to WS.');
-    wsClient.isAlive = true;
-    wsClient.on('pong', heartbeat);
+wss.on('connection', (wsClient) => {
+  wsClient.send('You are connected to WS.');
+  wsClient.isAlive = true;
+  wsClient.on('pong', heartbeat);
 
-    wsClient.on('message', (message) => {
-        console.log ('received message');
-        
-        wss.clients.forEach(client => {
-            client.send('Echo: ' + message);
-        })
+  wsClient.on('message', (message) => {
+    console.log('received message');
+
+    wss.clients.forEach(client => {
+      client.send('Echo: ' + message);
     })
+  })
 });
 
-//broadcast constantly
+// broadcast constantly
 // setInterval(() => {
-//     wss.clients.forEach(client => {
-//         client.send("Hi from server.");
-//     })
+//   wss.clients.forEach(client => {
+//     client.send("Hi from server.");
+//   })
 // }, 2000)
 
 //ping
 setInterval(() => {
-    wss.clients.forEach(client => {
-        if(client.isAlive === false) {
-            return client.terminate();
-        }
+  wss.clients.forEach(client => {
+    if (client.isAlive === false) {
+      return client.terminate();
+    }
 
-        client.isAlive = false;
-        client.ping(() => {});
-    })
+    client.isAlive = false;
+    client.ping(() => { });
+  })
 }, 10000)
 
 
@@ -130,13 +131,13 @@ server.on('stream', (stream, headers) => {
     console.log(`${name}: ${headers[name]}`);
   }
 
-  let id=0;
+  let id = 0;
 
   let receivedData = "";
 
   stream.respond({
     'content-type': 'text/event-stream',
-    'cache-control' : 'no-cache',
+    'cache-control': 'no-cache',
     // 'set-cookie' : '1P_JAR=2018-12-02-21; expires=Tue, 01-Jan-2019 21:47:55 GMT; path=/; domain=.google.com',
     ':status': 200,
   });
